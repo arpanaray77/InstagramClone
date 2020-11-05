831ba5a57257de16f369117695fe48c84de3f0ff
arpanaray77 <arpanaray75897@gmail.com>
2020-11-05 14:52:21 +0530

    Adding New Login Activity

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,10 +11,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,11 +41,21 @@ import java.util.List;
 import static android.widget.Toast.LENGTH_SHORT;
 
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity implements OnClickListener,View.OnKeyListener {
 
     Boolean signupModeActive = true;
     TextView login;
+    EditText username;
+    EditText password;
 
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        if(i==KeyEvent.KEYCODE_ENTER && keyEvent.getAction()==KeyEvent.ACTION_DOWN)
+        {
+            signup(view);
+        }
+        return false;
+    }
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.login) {
@@ -51,7 +65,13 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 signupModeActive = false;
                 signUp.setText("Login");
                 login.setText("Not yet Signed up? Signup");
-            } else {
+            }
+            //response is not invoked on clicking other parts of screen
+            else if (view.getId() == R.id.imageView || view.getId() == R.id.background) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            }
+            else {
                 signupModeActive = true;
                 signUp.setText("Sign Up");
                 login.setText("Already Signed Up? Login");
@@ -59,17 +79,34 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         }
     }
 
+    public void showUser() {
+        Intent intent=new  Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login = (TextView) findViewById(R.id.login);
         login.setOnClickListener(this);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        ImageView logoo=(ImageView)findViewById(R.id.imageView);
+        RelativeLayout background=(RelativeLayout)findViewById(R.id.background);
+        logoo.setOnKeyListener(this);
+        background.setOnKeyListener(this);
+        password.setOnKeyListener(this);
+
+        //if logged in go to login page
+        if(ParseUser.getCurrentUser()!=null)
+        {
+            showUser();
+        }
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
 
     public void signup(View view) {
-        EditText username = (EditText) findViewById(R.id.username);
-        EditText password = (EditText) findViewById(R.id.password);
+
         if (username.getText().toString().matches("") || password.getText().toString().matches(""))
             Toast.makeText(this, "Username and password are required", LENGTH_SHORT).show();
         else {
@@ -83,6 +120,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                     public void done(ParseException e) {
                         if (e == null) {
                             Log.i("Signup Done", "Yes");
+                            showUser();
                         } else
                             Toast.makeText(MainActivity.this, e.getMessage(), LENGTH_SHORT).show();
                     }
@@ -94,6 +132,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                     public void done(ParseUser user, ParseException e) {
                         if (user != null) {
                             Log.i("Login", "Logged In");
+                            showUser();
                            // Toast.makeText(MainActivity.this, "Username or Password incorrect", LENGTH_SHORT).show();
                         } else
                             Toast.makeText(MainActivity.this, "Username or Password incorrect", LENGTH_SHORT).show();
